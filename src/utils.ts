9e31d7f68fe75d2df83e1cd5cd454c404524f199
veehz <veehzhz@gmail.com>
2025-10-13 13:56:03 +0800

    fix: remove unused utils file

--- src/utils.ts
+++
@@ -1,7 +0,0 @@
-import { Tensor } from './tensor';
-
-export function ones_like(other: Tensor): Tensor {
-  const tensor = new Tensor(Array(other.data.length).fill(1));
-  tensor.shape = other.shape;
-  return tensor;
-}
