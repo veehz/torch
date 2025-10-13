6aa86b9a453e4cc96194f26471de3b360a5edf8a
veehz <veehzhz@gmail.com>
2025-10-13 16:29:28 +0800

    feat: use cpu

--- src/gpu.ts
+++ src/gpu.ts
@@ -11,7 +11,16 @@ import {
 
 export { Texture };
 
-const gpu = new GPU();
+/**
+ * Problems with gpu:
+ * - source array too large when all the options dynamicoutput dynamicarguments pipeline immutable are true
+ * - if pipeline is not used, then the memory transfer is too slow
+ * - if dynamicarguments is not used, then the source array is too large error will appear (https://github.com/gpujs/gpu.js/issues/495)
+ * - if dynamicoutputs is not used, then loops and broadcasting cannot be used
+ * 
+ * CPU is currently fast enough as well.
+ */
+const gpu = new GPU({ mode: 'cpu' });
 
 // gpu.addFunction(_get_original_index_kernel);
 gpu.addFunction(_get_original_index_kernel, {
