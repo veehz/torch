df60e5991f3d9a1d904a3aad7e24b860eaa9c49c
veehz <veehzhz@gmail.com>
2026-01-05 15:29:22 +0800

    feat: Adam Optimizer

--- src/optim/optimizers.ts
+++ src/optim/optimizers.ts
@@ -1,6 +1,7 @@
 import { Optimizer } from "./base";
 import { Parameter } from "../nn/module";
 import { Tensor } from "../tensor";
+import { zeros_like } from "../creation";
 
 export class SGD extends Optimizer {
   private state: Map<Parameter, { velocity: Tensor }> = new Map();
@@ -10,7 +11,7 @@ export class SGD extends Optimizer {
   private weight_decay: number;
   private nesterov: boolean;
   private maximize: boolean;
-  
+
   constructor(
     params: Parameter[],
     lr: number = 0.001,
@@ -32,11 +33,11 @@ export class SGD extends Optimizer {
   step(): void {
     for (const param of this.params) {
       let g = this.maximize ? param.grad.mul(-1) : param.grad;
-      if (this.weight_decay != 0) {
+      if (this.weight_decay !== 0) {
         g = g.add(param.mul(this.weight_decay));
       }
 
-      if (this.momentum != 0) {
+      if (this.momentum !== 0) {
         if (this.state.has(param)) {
           let buf = this.state.get(param)!.velocity;
           buf = buf.mul(this.momentum)
@@ -59,6 +60,84 @@ export class SGD extends Optimizer {
 
       // potentially unsafe?
       const newParam = param.sub(g.mul(this.lr));
+      param.data = newParam.data;
+    }
+  }
+}
+
+export class Adam extends Optimizer {
+  private state: Map<Parameter, {
+    m: Tensor,
+    v: Tensor,
+    vmax: Tensor
+  }> = new Map();
+
+  private step_count: number = 0;
+  private lr: number;
+  private beta1: number;
+  private beta2: number;
+  private eps: number;
+  private weight_decay: number;
+  private amsgrad: boolean;
+  private maximize: boolean;
+
+  constructor(
+    params: Parameter[],
+    lr: number = 0.001,
+    betas: [number, number] = [0.9, 0.999],
+    eps: number = 1e-8,
+    weight_decay: number = 0.0,
+    amsgrad: boolean = false,
+    maximize: boolean = false,
+  ) {
+    super(params, {});
+    this.lr = lr;
+    this.beta1 = betas[0];
+    this.beta2 = betas[1];
+    this.eps = eps;
+    this.weight_decay = weight_decay;
+    this.amsgrad = amsgrad;
+    this.maximize = maximize;
+  }
+
+  step(): void {
+    this.step_count += 1;
+    for (const param of this.params) {
+      let grad = this.maximize ? param.grad.mul(-1) : param.grad;
+
+      if (this.weight_decay !== 0) {
+        grad = grad.add(param.mul(this.weight_decay));
+      }
+
+      // Initialize
+      if (!this.state.has(param)) {
+        this.state.set(param, {
+          m: zeros_like(param),
+          v: zeros_like(param),
+          vmax: zeros_like(param),
+        });
+      }
+
+      const state = this.state.get(param)!;
+
+      state.m = state.m.mul(this.beta1).add(grad.mul(1 - this.beta1));
+      state.v = state.v.mul(this.beta2).add(grad.mul(grad).mul(1 - this.beta2));
+
+      const biasCorrection1 = 1 - Math.pow(this.beta1, this.step_count);
+      const biasCorrection2 = 1 - Math.pow(this.beta2, this.step_count);
+
+      let vhat: Tensor;
+      const mhat = state.m.div(biasCorrection1);
+      if (this.amsgrad) {
+        state.vmax = state.vmax.maximum(state.v);
+        vhat = state.vmax.div(biasCorrection2);
+      } else {
+        vhat = state.v.div(biasCorrection2);
+      }
+
+      const update = mhat.div(vhat.sqrt().add(this.eps)).mul(this.lr);
+
+      const newParam = param.sub(update);
       param.data = newParam.data;
     }
   }
--- test/optimizers.test.js
+++ test/optimizers.test.js
@@ -1,18 +1,74 @@
 import { assert } from 'chai';
 import * as torch from 'torch';
 
+const EPS = 1e-6;
+
 describe('Optimizers', () => {
   describe('SGD', () => {
     it('should update parameters', () => {
       const x = new torch.Tensor([1.0], { requires_grad: true });
+      const sgd = new torch.optim.SGD([x], 0.01);
+      
       const y = x.mul(new torch.Tensor(2.0));
       y.backward();
-
-      const sgd = new torch.optim.SGD([x], 0.01);
+      
+      /**
+       * x = 1, grad = 2
+       * x_new = x - lr * grad = 1 - 0.01 * 2 = 0.98
+       */
       sgd.step();
       const actual = sgd.params[0].data[0];
       const expected = 0.98;
-      assert.ok(Math.abs(actual - expected) < 1e-6);
+      assert.closeTo(actual, expected, EPS);
+    });
+
+    it('should update parameters correctly over multiple steps (quadratic)', () => {
+      const x = new torch.Tensor([0.5], { requires_grad: true });
+      const sgd = new torch.optim.SGD([x], 0.1);
+
+      let y = x.pow(2);
+
+      /**
+       * x = 0.5, grad = 2x = 1
+       * x_new = x - lr * grad = 0.5 - 0.1 * 1 = 0.4
+       */
+      y.backward();
+      sgd.step();
+      sgd.zero_grad();
+
+      let actual = x.data[0];
+      assert.closeTo(actual, 0.4, EPS);
+
+      /**
+       * x = 0.4, grad = 2x = 0.8
+       * x_new = x - lr * grad = 0.4 - 0.1 * 0.8 = 0.32
+       */
+      y = x.pow(2);
+      y.backward();
+      sgd.step();
+      
+      actual = x.data[0];
+      assert.closeTo(actual, 0.32, EPS);
+    });
+  });
+
+  describe('Adam', () => {
+    it('should handle bias correction with constant gradient', () => {
+      const x = new torch.Tensor([10.0], { requires_grad: true });
+      const adam = new torch.optim.Adam([x], 0.1);
+
+      let y = x.mul(2);
+      y.backward();
+      adam.step();
+      adam.zero_grad();
+
+      assert.closeTo(x.data[0], 9.9, EPS);
+
+      y = x.mul(2);
+      y.backward();
+      adam.step();
+
+      assert.closeTo(x.data[0], 9.8, EPS);
     });
   });
 });
